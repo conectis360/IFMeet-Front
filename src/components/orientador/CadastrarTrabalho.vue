8246b4907e0ef3d8623c1560f5b26446a841379d
James Garcia <conectis360@gmail.com>
2025-03-23 15:59:07 -0300

    feat: cadastrar trabalho page

--- src/components/orientador/CadastrarTrabalho.vue
+++ src/components/orientador/CadastrarTrabalho.vue
@@ -1,54 +1,85 @@
 <template>
-  <h1>Cadastrar Trabalho</h1>
-  <div class="container" style="background-color: #f4f6f9;">
+  <div class="container" style="background-color: #f4f6f9">
     <form>
       <div class="form-group">
         <label for="exampleFormControlInput1">Titulo Trabalho</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" v-model="TrabalhoDTO.titulo">
+        <input
+          type="text"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder=""
+          v-model="TrabalhoDTO.titulo"
+        />
       </div>
       <div class="form-group">
-        <label for="exampleFormControlSelect1">Aluno</label>
-        <select class="form-control" id="exampleFormControlSelect1" v-model="TrabalhoDTO.aluno.codigoUsuario">
-          <option v-for="aluno in AlunosDTO" :value="aluno.codigoUsuario" :key="aluno.codigoUsuario">
-            {{ aluno.nome }}
-        </option>
-        </select>
+        <div class="form-group">
+          <label for="exampleFormControlSelect1">Aluno</label>
+          <MySelect2
+            :options="AlunosDTO"
+            id="exampleFormControlSelect1"
+            label="Escolha uma opção"
+            placeholder="Selecione..."
+            @change="handleChangeAluno"
+            keyName="codigoUsuario"
+            valueName="nome"
+          />
+        </div>
       </div>
       <div class="form-group">
-        <label for="exampleFormControlSelect1">Curso</label>
-        <select class="form-control" id="exampleFormControlSelect1" v-model="TrabalhoDTO.curso.codigoCurso">
-          <option v-for="curso in CursosDTO" :value="curso.codigoCurso" :key="curso.codigoCurso">
-            {{ curso.nomeCurso }}
-        </option>
-        </select>
+        <div class="form-group">
+          <label for="exampleFormControlSelect1">Curso</label>
+          <MySelect2
+            :options="CursosDTO"
+            id="exampleFormControlSelect1"
+            label="Escolha uma opção"
+            placeholder="Selecione..."
+            @change="handleChangeCurso"
+            keyName="codigoCurso"
+            valueName="nomeCurso"
+          />
+        </div>
       </div>
       <div class="form-group">
         <label for="exampleFormControlSelect1">Orientador</label>
-        <select class="form-control" id="exampleFormControlSelect1" v-model="TrabalhoDTO.orientador.codigoUsuario">
-          <option v-for="orientador in OrientadoresDTO" :value="orientador.codigoUsuario" :key="orientador.codigoUsuario">
-            {{ orientador.nome }}
-        </option>
-        </select>
+        <MySelect2
+          :options="OrientadoresDTO"
+          id="exampleFormControlSelect1"
+          label="Escolha uma opção"
+          placeholder="Selecione..."
+          @change="handleChangeOrientador"
+          keyName="codigoUsuario"
+          valueName="nome"
+        />
       </div>
       <div class="form-group">
         <label for="exampleFormControlTextarea1">Descrição do Trabalho</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="12"></textarea>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="12"
+        ></textarea>
       </div>
     </form>
-    <div class="modal-footer justify-content-between">
-            <input type="submit" class="btn btn-success" value="Cadastrar Trabalho" @click="cadastrarTrabalho()">
-    </div>
-</div>
+  </div>
 </template>
-  
-<script>
-import { buscarCursos, cadastrarTrabalho, buscarAlunos, buscarOrientadores } from "@/services/cadastrarTrabalho.js";
-import { useToast } from "vue-toastification";
 
-const toast = useToast()
+<script>
+import {
+  buscarCursos,
+  cadastrarTrabalho,
+  buscarAlunos,
+  buscarOrientadores,
+} from "@/services/cadastrarTrabalho.js";
+import { useToast } from "vue-toastification";
+import MySelect2 from "../formularios/MySelect2.vue";
+
+const toast = useToast();
 
 export default {
   name: "CadastrarTrabalho",
+  components: {
+    MySelect2,
+  },
   data() {
     return {
       content: "",
@@ -65,82 +96,98 @@ export default {
       },
       CursosDTO: {},
       AlunosDTO: {},
+      OrientadoresDTO: [],
     };
   },
   mounted() {
-    this.retornarAlunos()
-    this.retornarOrientadores()
-    this.retornarCursos()
+    this.retornarAlunos();
+    this.retornarOrientadores();
+    this.retornarCursos();
   },
   methods: {
+    handleChangeOrientador(value) {
+      this.TrabalhoDTO.orientador.codigoUsuario = value;
+    },
+    handleChangeCurso(value) {
+      this.TrabalhoDTO.curso.codigoCurso = value;
+    },
+    handleChangeAluno(value) {
+      this.TrabalhoDTO.aluno.codigoUsuario = value;
+    },
     retornarCursos() {
       let loader = this.$loading.show({
         container: this.fullPage ? null : this.$refs.formContainer,
       });
-      buscarCursos((response) => {
-        if (response) {
-          loader.hide()
-          this.CursosDTO = response.data
-        }
-      },
+      buscarCursos(
+        (response) => {
+          if (response) {
+            loader.hide();
+            this.CursosDTO = response.data;
+          }
+        },
         (error) => {
-          loader.hide()
+          loader.hide();
           toast.error(error);
         },
-        () => { }
+        () => {}
       );
     },
     retornarAlunos() {
       let loader = this.$loading.show({
         container: this.fullPage ? null : this.$refs.formContainer,
       });
-      buscarAlunos((response) => {
-        if (response) {
-          loader.hide()
-          this.AlunosDTO = response.data.records
-        }
-      },
+      buscarAlunos(
+        (response) => {
+          if (response) {
+            loader.hide();
+            this.AlunosDTO = response.data.records;
+          }
+        },
         (error) => {
-          loader.hide()
+          loader.hide();
           toast.error(error);
         },
-        () => { }
+        () => {}
       );
     },
     retornarOrientadores() {
       let loader = this.$loading.show({
         container: this.fullPage ? null : this.$refs.formContainer,
       });
-      buscarOrientadores((response) => {
-        if (response) {
-          loader.hide()
-          this.OrientadoresDTO = response.data.records
-        }
-      },
+      buscarOrientadores(
+        (response) => {
+          if (response) {
+            loader.hide();
+            this.OrientadoresDTO = response.data.records;
+          }
+        },
         (error) => {
-          loader.hide()
+          loader.hide();
           toast.error(error);
         },
-        () => { }
+        () => {}
       );
     },
     cadastrarTrabalho() {
       let loader = this.$loading.show({
         container: this.fullPage ? null : this.$refs.formContainer,
       });
-      cadastrarTrabalho(this.TrabalhoDTO, (response) => {
-        if (response) {
-          loader.hide()
-          toast.success("Trabalho Cadastrado com Sucesso!");
-        }
-      },
+      cadastrarTrabalho(
+        this.TrabalhoDTO,
+        (response) => {
+          if (response) {
+            loader.hide();
+            toast.success("Trabalho Cadastrado com Sucesso!");
+          }
+        },
         (error) => {
-          loader.hide()
+          loader.hide();
           toast.error(error);
         },
-        () => { }
+        () => {}
       );
     },
   },
+  expose: ["cadastrarTrabalho"],
 };
 </script>
---
+++ src/pages/TrabalhoPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Página de Trabalhos</h1>
+    <button type="button" class="btn btn-success" @click="abrirModalCadastro">
+      Cadastrar Trabalho
+    </button>
+
+    <!-- Modal de Cadastro -->
+    <ModalComponent
+      ref="modalCadastro"
+      titulo="Cadastrar Trabalho"
+      @confirmar="cadastrarTrabalho"
+    >
+      <CadastrarTrabalho ref="formularioCadastro" />
+    </ModalComponent>
+  </div>
+</template>
+
+<script>
+import ModalComponent from "../components/basic/ModalComponent.vue"; // Importe o modal
+import CadastrarTrabalho from "../components/orientador/CadastrarTrabalho.vue";
+
+export default {
+  name: "TrabalhoPage",
+  components: {
+    ModalComponent,
+    CadastrarTrabalho,
+  },
+  data() {
+    return {
+      novoTrabalho: {
+        nome: "",
+        codigo: "",
+      },
+    };
+  },
+  methods: {
+    abrirModalCadastro() {
+      this.$refs.modalCadastro.abrirModal(); // Abre o modal
+    },
+    cadastrarTrabalho() {
+      // Acessa o componente CadastrarTrabalho e chama a função de cadastro
+      this.$refs.formularioCadastro.cadastrarTrabalho();
+    },
+  },
+};
+</script>
