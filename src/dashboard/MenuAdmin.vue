9b38375f219030d76be1b0a1a73821d8659f9065
James Garcia <conectis360@gmail.com>
2025-03-23 17:07:24 -0300

    fix: menus dashboard

--- src/dashboard/MenuAdmin.vue
+++ src/dashboard/MenuAdmin.vue
@@ -1,47 +1,41 @@
 <template>
-    <a href="#" class="nav-link active">
-      <i class="nav-icon fas fa-tachometer-alt"></i>
-      <p>
-        Menu Admin
-        <i class="right fas fa-angle-left"></i>
-      </p>
-    </a>
-    <ul class="nav nav-treeview">
-      <li class="nav-item">
-        <registrar-usuario/>
-      </li>
-      <li class="nav-item">
-        <router-link to="/reunioes" class="nav-link">
-          <i class="fa fa-calendar"></i>
-            <p class="opcaoMenu">Reuniões</p>
-        </router-link>
-      </li>
-      <li class="nav-item">
-        <router-link to="/trabalhos" class="nav-link">
-          <i class="fa fa-book"></i>
-            <p class="opcaoMenu">Trabalhos</p>
-        </router-link>
-      </li>
-      <li class="nav-item">
-        <router-link to="/cadastrarTrabalho" class="nav-link">
-          <i class="fa fa-book"></i>
-            <p class="opcaoMenu">Cadastrar Trabalho</p>
-        </router-link>
-      </li>
-      <li class="nav-item">
-        <router-link to="/tarefas" class="nav-link">
-          <i class="fa fa-pen"></i>
-            <p class="opcaoMenu">Tarefas</p>
-        </router-link>
-      </li>
-    </ul>
+  <a href="#" class="nav-link active">
+    <i class="nav-icon fas fa-tachometer-alt"></i>
+    <p>
+      Menu Admin
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <registrar-usuario />
+    </li>
+    <li class="nav-item">
+      <router-link to="/reunioes" class="nav-link">
+        <i class="fa fa-calendar"></i>
+        <p class="opcaoMenu">Reuniões</p>
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/trabalhos" class="nav-link">
+        <i class="fa fa-book"></i>
+        <p class="opcaoMenu">Trabalhos</p>
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/tarefas" class="nav-link">
+        <i class="fa fa-pen"></i>
+        <p class="opcaoMenu">Tarefas</p>
+      </router-link>
+    </li>
+  </ul>
 </template>
 <script>
 import RegistrarUsuario from "@/components/RegistrarUsuario";
 export default {
   name: "MenuAdmin",
   components: {
-    RegistrarUsuario
+    RegistrarUsuario,
   },
   computed: {
     currentUser() {
