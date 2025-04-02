1934f337b2936a6cd92ec29e4de9123213494c88
James Garcia <conectis360@gmail.com>
2025-04-01 22:13:33 -0300

    feat: api

--- src/services/API.js
+++ src/services/API.js
@@ -3,16 +3,14 @@ import axios from 'axios'
 function axiosRequestSuccess(response) {
     return response
 }
-
 const timeoutRequests = 600000
-
 export const apiIFMeet = axios.create({
-    baseURL: "https://ifmeet.sytes.net:8443/api/v1/",  // Alterado para HTTPS e caminho via Nginx
+    baseURL: "https://ifmeet.sytes.net:8443/api",
+    withCredentials: true,
     timeout: timeoutRequests,
     headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true,  // Mantido para envio de cookies/sessão
+        'Content-Type': 'application/json'
+    }
 })
 
 apiIFMeet.interceptors.response.use(axiosRequestSuccess)
--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,21 @@
 module.exports = {
   publicPath: '/',
   devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://ifmeet.sytes.net:9000',
-        changeOrigin: true,
-        secure: false
-      },
-      port: 8081
+    port: 8081
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].[hash].js',
+      chunkFilename: '[name].[hash].js',
     },
-    configureWebpack: {
-      output: {
-        filename: '[name].[hash].js',
-        chunkFilename: '[name].[hash].js',
-      },
-    },
+  },
 
-    pluginOptions: {
-      i18n: {
-        locale: 'en',
-        fallbackLocale: 'en',
-        localeDir: 'locales',
-        enableInSFC: false,
-      },
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
     },
-  }
+  },
 }
