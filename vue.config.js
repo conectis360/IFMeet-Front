2632aea57b44f76af93d448b5f4de43d70250fc9
James Garcia <conectis360@gmail.com>
2025-04-01 22:03:13 -0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,27 @@
 module.exports = {
   publicPath: '/',
   devServer: {
-    port: 8081
-  },
-  configureWebpack: {
-    output: {
-      filename: '[name].[hash].js',
-      chunkFilename: '[name].[hash].js',
+    proxy: {
+      '/api': {
+        target: 'http://ifmeet.sytes.net:9000',
+        changeOrigin: true,
+        secure: false
+      },
+      port: 8081
     },
-  },
+    configureWebpack: {
+      output: {
+        filename: '[name].[hash].js',
+        chunkFilename: '[name].[hash].js',
+      },
+    },
 
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false,
+    pluginOptions: {
+      i18n: {
+        locale: 'en',
+        fallbackLocale: 'en',
+        localeDir: 'locales',
+        enableInSFC: false,
+      },
     },
-  },
-}
+  }
