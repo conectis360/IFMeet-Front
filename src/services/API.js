ee55c185dd3371380d0e489d20f15e24023fd672
James Garcia <conectis360@gmail.com>
2025-04-02 22:48:40 -0300

    fix: api calls and envs

--- src/services/API.js
+++ src/services/API.js
@@ -1,16 +1,18 @@
-import axios from 'axios'
+import axios from 'axios';
 
 function axiosRequestSuccess(response) {
-    return response
+    return response;
 }
-const timeoutRequests = 600000
+
+const timeoutRequests = 600000;
+
 export const apiIFMeet = axios.create({
-    baseURL: "https://ifmeet.sytes.net:8443/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL, // Pega do .env
     withCredentials: true,
     timeout: timeoutRequests,
     headers: {
         'Content-Type': 'application/json'
     }
-})
+});
 
-apiIFMeet.interceptors.response.use(axiosRequestSuccess)
+apiIFMeet.interceptors.response.use(axiosRequestSuccess);
