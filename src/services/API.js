2394629921b8357f6cea1dbbc64a475c64ed6006
James Garcia <conectis360@gmail.com>
2025-04-23 15:53:17 -0300

    fix: endereco

--- src/services/API.js
+++ src/services/API.js
@@ -8,7 +8,7 @@ const timeoutRequests = 600000;
 
 export const apiIFMeet = axios.create({
     //baseURL: import.meta.env.VITE_API_BASE_URL, // Pega do .env
-    baseURL: "http://ifmeet.sytes.net:8443/api/",
+    baseURL: "https://ifmeet.sytes.net:8443/api/",
     //baseURL: "http://localhost:9000/v1/",
     withCredentials: true,
     timeout: timeoutRequests,
